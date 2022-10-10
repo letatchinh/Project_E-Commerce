78175dda91c1eb21dc5e54713de8f968eb248252
letatchinh <106223267+letatchinh@users.noreply.github.com>
2022-10-10 20:37:11 +0700

    j

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,11 +5,12 @@ import createSagaMiddleware from 'redux-saga'
 import mySaga from './sagas/Mysaga';
 
 const sagaMiddleware = createSagaMiddleware()
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk,sagaMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : []
+    (window.__REDUX_DEVTOOLS_EXTENSION__) ? window.__REDUX_DEVTOOLS_EXTENSION__() : composeEnhancers
   )
 );
 export default store;
