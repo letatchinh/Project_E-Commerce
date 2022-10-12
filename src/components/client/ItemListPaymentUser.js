fb2397712758d15319a745c0feaa2401246ff61c
letatchinh <106223267+letatchinh@users.noreply.github.com>
2022-10-12 19:00:16 +0700

    j

--- src/components/client/ItemListPaymentUser.js
+++ src/components/client/ItemListPaymentUser.js
@@ -1,19 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import { Button, Card, CardActionArea, CardContent, CardMedia, Stack, Typography } from '@mui/material'
-import axios from 'axios'
-import { URL_BASE } from '../../constant/UrlConstant'
-import PriceSell from './PriceSell'
-import { useSelector } from 'react-redux'
+import React from 'react'
+import { Button,  Stack, Typography } from '@mui/material'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import '../StyleComponent/ItemListPayment.css'
+import { fetchAddToCartRequestSaga } from '../../redux/sagas/Mysaga'
+import { KEY_USER } from '../../constant/LocalStored'
 export default function ItemListPaymentUser({data}) {
   const mainColorText = useSelector(state => state.colorCommon.mainColorText)
+  const idUser = JSON.parse(localStorage.getItem(KEY_USER))
 const {images,name , price,_id}  = data
-const handleBuyAgain = () => {
-  console.log("ok");
+const dispatch = useDispatch()
+const handleBuyAgain = (e) => {
+  // e.stopPropagation()
+  dispatch(
+    fetchAddToCartRequestSaga({
+product: _id,
+user: idUser,
+})
+)
 }
   return (
-    <Link to={`/products/${_id}`}>
+    <Link to={`/products/${_id}`} >
     <Stack className='itemListPayment' direction='row' spacing={3} borderBottom='1px solid #CACACA' padding='10px'>
     
     <img style={{width : '100px',height : '100px',objectFit : 'cover'}} src={`/images/${images[0]}`} alt='22'/>
