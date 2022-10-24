30edbad838c8fbfe5cf40e07d1436c5e654e77d9
Cuong <cuonghuynh936@gmail.com>
2022-10-24 10:53:00 +0700

    fix

--- set-up-server/data/Orderuser.js
+++ set-up-server/data/Orderuser.js
@@ -1,26 +1,24 @@
 const ordersUser = [
-   
-    {
-      user: "632ec5d1033cf01c5f78029f",
-      orderItem: [
-        {
-          name: "zczxc",
-          qty: 12,
-          image: "img11.png",
-          price: 10,
-          product: "633e65d4d687892aba076da0",
-        },
-      ],
-      shippingAddress: {
-        address: "kkk",
-        city: "aa",
-        postalCode: "POX : 12233",
-        country: "aaa",
+  {
+    user: "632ec5d1033cf01c5f78029f",
+    orderItem: [
+      {
+        name: "zczxc",
+        qty: 12,
+        image: "img11.png",
+        price: 10,
+        product: "633e65d4d687892aba076da0",
       },
-      taxPrice: 123,
-      totalPrice: 100,
+    ],
+    shippingAddress: {
+      address: "kkk",
+      city: "aa",
+      postalCode: "POX : 12233",
+      country: "aaa",
     },
-  ];
-  
-  export default ordersUser;
-  
+    taxPrice: 123,
+    totalPrice: 100,
+  },
+];
+
+export default ordersUser;
