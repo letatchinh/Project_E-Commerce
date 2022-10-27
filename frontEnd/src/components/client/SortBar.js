c8cdf3ffbb32f5698fe918455cbf1eeb79d75753
letatchinh <106223267+letatchinh@users.noreply.github.com>
2022-10-27 21:56:01 +0700

    fix sort

--- frontEnd/src/components/client/SortBar.js
+++ frontEnd/src/components/client/SortBar.js
@@ -1,40 +1,49 @@
 import { FormControl, InputLabel, MenuItem, Select, Typography } from '@mui/material'
 import { Stack } from '@mui/system'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { setSortPrice, setSortRating} from '../../redux/filterProduct/Actions'
 import '../StyleComponent/SideBarFilter.css'
 
 export default function SortBar({setPage}) {
   const mainColorText = useSelector(state => state.colorCommon.mainColorText)
+  const sortPrice = useSelector((state) => state.filterProduct.sortPrice);
+  const sortRating = useSelector((state) => state.filterProduct.sortRating);
   const dispatch = useDispatch()
   const [price, setPrice] = useState('');
+  const [rating, setRating] = useState('');
+  useEffect(() => {
+    setPrice(sortPrice || '')
+    setRating(sortRating || '')
+  },[sortPrice,sortRating])
+  console.log(price,'price');
+  console.log(rating,'rating');
   const handleChangePrice = (event) => {
     setPrice(event.target.value);
-    if(event.target.value){
+    if(event.target.value === -1){
       dispatch(setSortPrice(-1))
       setPage()
     }
-    else if(event.target.value === false){
+    else if(event.target.value === 1){
       dispatch(setSortPrice(1))
       setPage()
     }
-    else{
+    else if(event.target.value === ''){
       dispatch(setSortPrice(null))
       setPage()
     }
   };
-  const [rating, setRating] = useState('');
+
   const handleChangeRating = (event) => {
     setRating(event.target.value);
-    if(event.target.value){
+    if(event.target.value === -1){
       dispatch(setSortRating(-1))
       setPage()
     }
-    else if(event.target.value === false){
+    else if(event.target.value === 1){
       dispatch(setSortRating(1))
       setPage()
-    }else{
+    }else if(event.target.value === ''){
       dispatch(setSortRating(null))
       setPage()
 
@@ -52,9 +61,9 @@ export default function SortBar({setPage}) {
           value={price}
           onChange={handleChangePrice}
         >
-          <MenuItem value={true}>High to low</MenuItem>
-          <MenuItem value={false}>Low to High</MenuItem>
-          <MenuItem value={null}>None</MenuItem>
+          <MenuItem value={-1}>High to low</MenuItem>
+          <MenuItem value={1}>Low to High</MenuItem>
+          <MenuItem value={''}>None</MenuItem>
         </Select>
       </FormControl>
     <FormControl sx={{width : {md : '200px' , sm : '100px' , xs : '100px'}  , background : 'white'}}>
@@ -66,9 +75,9 @@ export default function SortBar({setPage}) {
           value={rating}
           onChange={handleChangeRating}
         >
-          <MenuItem value={true}>High to low</MenuItem>
-          <MenuItem value={false}>Low to High</MenuItem>
-          <MenuItem value={null}>None</MenuItem>
+          <MenuItem value={-1}>High to low</MenuItem>
+          <MenuItem value={1}>Low to High</MenuItem>
+          <MenuItem value={''}>None</MenuItem>
         </Select>
       </FormControl>
    </Stack>
